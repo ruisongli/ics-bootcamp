052af27f0950703258bbbf233d0474730bf80289
Ruisong Li <RuisongLi@Ruisongs-MacBook-Air.local>
2017-12-22 18:15:29 -0500

    ch02

---
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,3 @@
+puts 'What\'s your favorite number?'
+better_number= gets.chomp.to_i + 1
+puts better_number.to_s + ' is larger therefore it\'s a better number ;)'
---
+++ ch05/fullNameGreeting.rb
@@ -0,0 +1,7 @@
+puts 'Hello there! What is your first name？'
+first_name = gets.chomp
+puts 'Nice to meet you ' + first_name + '! What about your middle name'
+middle_name = gets.chomp
+puts 'I am sorry, forgot to ask for your last name. What is it?'
+last_name = gets.chomp
+puts 'Hello ' + first_name + ' ' + middle_name + ' ' + last_name + '! What a beautiful name!'
